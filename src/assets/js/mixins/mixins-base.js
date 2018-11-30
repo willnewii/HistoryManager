b2b939fece6c8b84f203c723dda49ccd0779015e
诗人的咸鱼 <willnewii@163.com>
2018-11-30 14:35:49 +0800

    change：封装请求

--- src/assets/js/mixins/mixins-base.js
+++ src/assets/js/mixins/mixins-base.js
@@ -1,3 +1,6 @@
+import mockData from '../../../mock';
+import * as util from '../util';
+
 export default {
     beforeRouteEnter(to, from, next) {
         //页面未创建,不能访问this
@@ -21,6 +24,63 @@ export default {
         };
     },
     methods: {
+        //获取历史记录
+        getHistory(option) {
+            if (!chrome.history) {
+                option.callback && option.callback(mockData.visitData.slice(0, 50));
+                return;
+            }
+
+            option.text = option.text || '';
+            option.startTime = option.startTime || 0;
+            option.maxResults = option.maxResults || 100;
+
+            let visits = [];
+            let getVisitsCount = 0;
+
+            chrome.history.search({
+                    'text': option.text,
+                    'startTime': option.startTime || 0,
+                    'endTime': option.endTime,
+                    'maxResults': option.maxResults
+                },
+                (historyItems) => {
+                    getVisitsCount = historyItems.length;
+
+                    if (getVisitsCount === 0) {
+                        option.callback && option.callback(visits);
+                        return;
+                    }
+
+                    for (let item of historyItems) {
+                        chrome.history.getVisits({
+                            url: item.url
+                        }, (lists) => {
+                            getVisitsCount--;
+                            let results = lists.filter((value) => {
+                                value.url = item.url;
+                                value.title = item.title;
+                                value.visitCount = item.visitCount;
+                                return (value.visitTime >= option.startTime && value.visitTime < option.endTime);
+                            });
+                            //visits = visits.concat(results);
+                            //只显示时间段内最近的一条
+                            visits.push(results[results.length - 1]);
+                            if (getVisitsCount === 0) {
+
+                                /*visits.sort(function (a, b) {
+                                    return b.visitTime - a.visitTime;
+                                });*/
+
+                                for (let i = 0; i < visits.length; i++) {
+                                    visits[i].date = util.formatTime(visits[i].visitTime);
+                                }
+                                option.callback && option.callback(visits);
+                            }
+                        });
+                    }
+                });
+        },
         pagePause() {
             this._pagePause && this._pagePause();
         },
